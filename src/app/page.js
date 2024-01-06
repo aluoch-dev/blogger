4e6a335495d09e05df200bb0d1d4be67c81a2450
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-06 14:35:11 +0300

    Add header text

--- src/app/page.js
+++ src/app/page.js
@@ -9,8 +9,13 @@ export default function Home() {
   return (
     <>
       <Navbar logo={logo}/>
-      <div class="container mx-auto p-4">
-        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      <div className="container mx-auto p-8">
+        <h6 className="text-black font-bold text-3xl text-start mt-2">The Accessibility Blog</h6>
+        <h3 className="text-black text-base text-start">The voice of the excluded</h3>
+
+      </div>
+      <div className="container mx-auto p-4 m">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
           {blogData?.map((blogDataItem, index) => (
             <BlogItem 
               key={index}
