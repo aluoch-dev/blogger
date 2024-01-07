61a0da1da79ace2439cd7107d6f25eb02511ab11
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-07 10:57:24 +0300

    Update Blogs page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,8 +5,9 @@ import blogData from "@/data/blogdata";
 export default function Blog() {
   return (
     <>
-      <div className="container mx-auto p-8 align-center justify-center">
-        <SearchInput />
+      <div className="container mx-auto p-8">
+        <h6 className="text-black font-bold text-3xl text-center mt-2">The Accessibility Blog</h6>
+        <h3 className="text-black text-base text-center">The voice of the excluded</h3>
       </div>
       <div className="container mx-auto p-4 m">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -3,7 +3,6 @@ import Link from 'next/link';
 
 export const BlogCard = ({slug, image, title, date, readTime, content}) => {
 
-    
     return (
         <Link href={`/blog/${slug}`} className="max-w-sm overflow-hidden m-4 bg-gray-100">            <Image class="w-full" src={image} alt="" />
             <div className="px-6 py-4 bg-gray-100">
