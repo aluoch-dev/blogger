7622f4ca39265a6e8ea7b513cffcece4c4e6b734
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-07 10:35:01 +0300

    introduce blog folder and add slug folder for navigation

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -5,7 +5,7 @@ export const BlogCard = ({slug, image, title, date, readTime, content}) => {
 
     
     return (
-        <Link href={`/${slug}`} className="max-w-sm overflow-hidden m-4 bg-gray-200">            <Image class="w-full" src={image} alt="" />
+        <Link href={`/blog/${slug}`} className="max-w-sm overflow-hidden m-4 bg-gray-200">            <Image class="w-full" src={image} alt="" />
             <div class="px-6 py-4 bg-gray-100">
                 <h1><span>{date}</span> . <span>{readTime} min Read</span></h1>
             <h2 class="font-bold text-l mb-2">{title }</h2>
