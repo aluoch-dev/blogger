af8e01ffd07646840a6a2cc8706b8f7d9da9c4d1
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-07 10:48:07 +0300

    Update Text size for Date and Read time

--- src/components/blogCard.js
+++ src/components/blogCard.js
@@ -6,12 +6,12 @@ export const BlogCard = ({slug, image, title, date, readTime, content}) => {
     
     return (
         <Link href={`/blog/${slug}`} className="max-w-sm overflow-hidden m-4 bg-gray-100">            <Image class="w-full" src={image} alt="" />
-            <div class="px-6 py-4 bg-gray-100">
-                <h1><span>{date}</span> . <span>{readTime} min Read</span></h1>
-            <h2 class="font-bold text-l mb-2">{title }</h2>
-            <p class="text-black text-base">
-                {content.substring(0, 150)}...
-            </p>
+            <div className="px-6 py-4 bg-gray-100">
+                <h1 className="text-xs"><span>{date}</span> . <span>{readTime} min Read</span></h1>
+                <h2 className="font-bold text-l mb-2">{title }</h2>
+                <p className="text-black text-base">
+                    {content.substring(0, 150)}...
+                </p>
             </div>
       </Link>
     )
