7cc34871b4dfe7a8b003e15f665bc7a188c3fe2e
Aluoch Laurine <laurinealuoch7@gmail.com>
2024-01-07 12:11:07 +0300

    Update Navigation responsiveness

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,24 +5,36 @@ import { SearchInput } from "./inputs";
 
 export const Navbar = ({logo}) => {
     return (
-        <nav className="bg-gray-300 p-4">
-            <div className="container mx-auto flex justify-between items-center">
+        <>
+        <nav className="flex items-center justify-between flex-wrap bg-gray-300 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <Link className="flex items-center" href="/">
                 <Image src={logo} alt="Logo" className="h-8" />
                 </Link>
-
-                <div className="links flex">
-                    <Link 
-                    className="px-2 py-2"
-                    href="/blog">Blogs</Link>
-                    <SearchInput />
-                    <Button 
-                    className="px-2 py-2"
-                    title="Create Blog Post"
-                    onClick=""
-                    />
-                </div>
             </div>
-        </nav>      
+        <div className="block lg:hidden md:hidden">
+            <button className="flex items-center px-3 py-2 border rounded text-black border-black hover:text-white hover:border-white">
+            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+            </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto md:flex md:items-center md:w-auto">
+            <div className="text-sm lg:flex-grow md:flex-grow"></div>
+            <div>
+            <Link 
+                className="px-2 py-2 hover:text-white block mt-4 lg:inline-block lg:mt-0 md:inline-block md:mt-0"
+                href="/blog">Blogs</Link>
+            {/* <SearchInput
+                className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0"
+            /> */}
+            <Button 
+                className="px-2 py-2 block mt-4 lg:inline-block lg:mt-0 "
+                title="Create Blog Post"
+                onClick=""
+            />
+            </div>
+        </div>
+        </nav>
+        </>
+
     );
 }
